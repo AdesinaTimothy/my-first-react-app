1dbf3e6ec49775706a65e125768f091567ab95d5
Timothy Adesina <timobest121@gmail.com>
2024-10-18 11:21:24 +0100

    Initial Commit

---
+++ src/Card.jsx
@@ -0,0 +1,26 @@
+
+function Card () {
+    return (
+        <div className="card-container">
+            <div className="card-title-container">
+                <h1>Card Title</h1>
+            </div>
+
+            <div className="card-body-container">
+                <div className="card-description-container">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Id quae consequatur voluptatem reprehenderit mollitia 
+                        obcaecati 
+                        molestias, cumque distinctio saepe laboriosam fugiat, commodi provident 
+                        harum qui aliquid facere iste adipisci deserunt.</p>
+                </div>
+
+                <div className="card-button-container">
+                    <button>Suscribe</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
